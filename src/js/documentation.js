18f17dd0558a63354ec74ad88d43cb55a05de5da
Blazzike <blazzike@gmail.com>
2018-09-27 19:48:12 -0400

    Redesign PaperMC Website

---
+++ src/js/documentation.js
@@ -0,0 +1,212 @@
+var documentationFileToTitle = {}, documentation =
+{
+  "PaperMC Documentation": {
+    "file": "index.html"
+  },
+  "Running A Server": {
+    "file": "server/index.html",
+    "Getting Started": {
+      "file": "server/getting-started.html"
+    },
+    "Configuration": {
+      "file": "server/configuration.html"
+    },
+  },
+  "Contributing": {
+    "file": "contributing/index.html"
+  },
+  "About the PaperMC Project": {
+    "file": "about/index.html",
+    "Introduction": {
+      "file": "about/introduction.html"
+    },
+    "Frequently Asked Questions": {
+      "file": "about/faq.html"
+    },
+    "The Structure of PaperMC": {
+      "file": "about/structure.html"
+    },
+    "Art Assets": {
+      "file": "about/assets.html"
+    },
+    "Contact Us": {
+      "file": "about/contact.html"
+    },
+  }
+};
+
+function addSection(section) {
+  var html = "";
+  for (var subsection in section) {
+    var length = Object.keys(section[subsection]).length;
+    if (length == 0 || subsection == "file")
+      continue;
+    
+    documentationFileToTitle[section[subsection].file] = subsection;
+    
+    // Page
+    if (length == 1) {
+      html += '<li><a href="#' + subsection.replace(/ /g, '_') + '" data-file="' + section[subsection].file + '">' + subsection + '</a></li>';
+      
+      continue;
+    }
+    
+    // Category
+    var sectionsHTML = addSection(section[subsection]);
+    html += `
+      <li>
+        <ul class="collapsible collapsible-accordion">
+          <li>
+            <a class="collapsible-header" href="#${subsection.replace(/ /g, '_')}" data-file="${section[subsection].file}">${subsection}</a>
+            <div class="collapsible-body">
+              <ul>
+                ${sectionsHTML}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </li>
+    `;
+  }
+  
+  return html;
+}
+
+function loadDocumentation(file) {
+  container = document.querySelector("#content>.container");
+  
+  // Loader
+  container.innerHTML = '<div class="progress"><div class="indeterminate"></div></div>';
+  
+  // Load
+  window.fetch("/documentation/sphinx/" + file).then(function(response) {
+    if (response.status !== 200) {
+      container.innerText = "Something went wrong, how embarrassing.";
+      
+      return;
+    }
+    
+    response.text().then(function(text) {
+      var xml = text, html = new DOMParser().parseFromString(xml, "text/xml");
+      
+      // Fix image relativity
+      html.querySelectorAll("img[src]:not([src^='http'])").forEach(function(img) {
+        src = img.getAttribute("src");
+        img.src = "/documentation/sphinx/" + (src.startsWith('../') ? src.substr(3) : src);
+      });
+      
+      // Fix href relativity
+      
+      html.querySelectorAll("*[href]:not([href^='http'])").forEach(function(el) {
+        href = el.getAttribute("href");
+        hrefRelative = href.startsWith('../') ? href.substr(3) : href;
+        for (var file in documentationFileToTitle) {
+          if (file == hrefRelative || file == file.substr(0, file.indexOf("/")) + "/" + hrefRelative) {
+            el.href = "#" + documentationFileToTitle[file].replace(/ /g, '_');
+            
+            break;
+          }
+        }
+      });
+      
+      // Remove title self anchors
+      html.querySelectorAll("a.toc-backref").forEach(function(el) {
+        el.removeAttribute("href");
+      });
+      
+      container.innerHTML = html.querySelector(".body[role='main']").innerHTML;
+      anchorScrollHandler();
+      
+      // Active
+      document.querySelectorAll("#sidenav li").forEach(function(el) {
+        el.classList.remove("active");
+        var a = el.querySelector("a");
+        if (a == null)
+          return;
+        
+        if (a.getAttribute("data-file") == file) {
+          el.classList.add("active");
+        }
+      });
+    });
+  });
+}
+
+var anchorClickHandler = function(e) {
+  if (this.hash.length == 0) {
+      window.scroll({ 
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+      });
+      
+      e.preventDefault();
+      return;
+  }
+  
+  el = document.getElementById(this.hash.substr(1));
+  if (el == null)
+    return;
+
+  el.goTo();
+  e.preventDefault();
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    anchorScrollHandler = function() {
+      document.querySelectorAll('a[href*="#"]').forEach(function(el) {
+        el.removeEventListener('click', anchorClickHandler);
+        el.addEventListener('click', anchorClickHandler);
+      });
+    }
+    
+    anchorScrollHandler();
+    
+    // Documentation Sidenav
+    document.getElementById("sidenav").innerHTML += addSection(documentation);
+                
+    /*document.querySelectorAll('a[data-file]').forEach(function(el) {
+      el.addEventListener('click', function(e) {
+        file = this.getAttribute("data-file");
+        loadDocumentation(file);
+        document.getElementById("documentation").goTo();
+      });
+    });*/
+    
+    // Material initialization
+    M.Collapsible.init(document.querySelectorAll('.collapsible'), {});
+    
+    // Load first documentation page
+    var hashFile = location.hash.substring(1).toLowerCase().replace(/_/g, ' ');
+    for (file in documentationFileToTitle)
+      if (hashFile == documentationFileToTitle[file].toLowerCase()) {
+        document.getElementById("documentation").goTo();
+        loadDocumentation(file);
+        
+        return;
+      }
+      
+    loadDocumentation("index.html");
+});
+
+window.addEventListener("hashchange", function(e) {
+  var hash = location.hash.substring(1), hashFile = hash.toLowerCase().replace(/_/g, ' ');
+  for (file in documentationFileToTitle)
+    if (hashFile == documentationFileToTitle[file].toLowerCase()) {
+      loadDocumentation(file);
+      if (window.innerWidth < 992)
+        M.Sidenav.getInstance(document.querySelector("#sidenav")).close();
+      
+      document.getElementById("documentation").goTo();
+      e.preventDefault();
+      
+      return;
+    }
+    
+  // Not a page, see if the element is around
+  el = document.getElementById(hash);
+  if (el === null)
+    return;
+  
+  el.goTo();
+});
--- src/js/downloads.js
+++ src/js/downloads.js
@@ -1,74 +1,107 @@
-let JENKINS_LEGACY_URL = "/ci/job/Paper/";
-let JENKINS_113_URL = "/ci/job/Paper-1.13/";
-Vue.component('download-container', {
-    template: '#download-container-template'
-});
-function fetchJenkinsBuilds(baseUrl) {
-    return window.fetch(
-        baseUrl + "/api/json?tree=builds[number,url,artifacts[fileName,relativePath],timestamp]{,5}"
-    )
-    .then(function(resp) {
-        if (resp.status !== 200) {
-            throw new Error("unexpected error status " + resp.status)
-        }
-        return resp.json();
-    });
+const downloads = {
+  "Paper 1.13.1": {
+    "id": "Paper-1.13",
+    "github": "PaperMC/Paper"
+  },
+  "Paper 1.12.2": {
+    "id": "Paper",
+    "github": "PaperMC/Paper"
+  },
+  "Waterfall": {
+    "id": "Waterfall",
+    "github": "WaterfallMC/Waterfall"
+  }
+};
+
+function jenkinsFetch(job, path) {
+  return window.fetch("https://papermc.io/ci/job/" + job + path).then(function(response) {
+    if (response.status !== 200)
+      return null;
+    
+    return response.json();
+  });
 }
 
-function onLoad() {
-    function buildChanges(baseUrl, target) {
-        var app = new Vue({
-            el: target,
-            data: {
-                loading: true,
-                builds: []
-            }
+function escapeHTML(str) {
+  return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
+ }
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Show downloads
+  var tabs = "", tabContents = "", jobs = Object.keys(downloads).length;
+  for (var title in downloads) {
+    tabs += `<li class="tab col s${12 / jobs}"><a href="#${downloads[title].id}">${title}</a></li>`;
+    tabContents += `<div id="${downloads[title].id}" class="col s12">
+      <div class="progress">
+          <div class="indeterminate"></div>
+      </div>
+    </div>`;
+    
+    let id = downloads[title].id;
+    let githubID = downloads[title].github;
+    jenkinsFetch(id, "/api/json?tree=builds[number,url,artifacts[fileName,relativePath],timestamp]{,5}").then(function(json) {
+      var container = document.getElementById(id);
+      if (json == null) {
+        container.innerText = "Failed to load downloads.";
+        
+        return;
+      }
+      
+      var rows = "";
+      json.builds.forEach(function(build) {
+        rows += `
+        <tr>
+          <td><a href="https://papermc.io/ci/job/${id}/${build.number}/artifact/${build.artifacts[0].fileName}" class="btn waves-light waves-effect grey darken-4">#${build.number}<i class="material-icons left">save_alt</i></a></td>
+          <td data-build-id="${build.number}">Loading...</td>
+          <td>${new Date(build.timestamp).toLocaleDateString({year: "numeric", month: "numeric", day: "numeric"})}</td>
+        </tr>`;
+        
+        jenkinsFetch(id, "/" + build.number + "/api/json?wrapper=changes").then(function(json) {
+          var el = document.querySelector("td[data-build-id='" + build.number + "']");
+          if (json == null) {
+            el.innerText = "Failed to load downloads.";
+            
+            return;
+          }
+          
+          var changes = "";
+          json.changeSet.items.forEach(function(item) {
+            changes += `[<a title="${escapeHTML(item.comment)}" href="https://github.com/${githubID}/commit/${item.commitId}" target="_blank">${escapeHTML(item.commitId.substring(0, 7))}</a>] ${escapeHTML(item.msg)}<br>`;
+          });
+          
+          el.innerHTML = changes.substr(0, changes.length - 1);
+        }).catch(function() {
+          document.querySelector("td[data-build-id='" + build.number + "']").innerText = "Failed to load downloads.";
         });
+      });
+      
+      container.innerHTML = `
+      <table class="builds-table">
+        <thead>
+          <tr>
+            <th>Build</th>
+            <th>Changes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
 
-        fetchJenkinsBuilds(baseUrl).then(function (jenkinsResult) {
-            // Lightly process the result
-            jenkinsResult.builds.forEach(function (build, i) {
-                build.latest = i === 0;
-                build.formattedDate = new Date(build.timestamp).toLocaleDateString(undefined, {
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric"
-                });
-                window.fetch(baseUrl + '/' + build.number + '/api/json?wrapper=changes').then((changesResp) => {
-                    return changesResp.json();
-                }).then((changes) => {
-                    const changeList = [];
-                    changes.changeSet.items.forEach(item => {
-                        if (item.comment.toUpperCase().indexOf('[CI-SKIP]') !== -1) return;
-                        changeList.push({
-                            title: item.comment.split(/\n/)[0],
-                            text: item.comment,
-                            url: 'https://github.com/PaperMC/Paper/commit/' + item.commitId,
-                            id: item.commitId.substring(0, 7)
-                        });
-                    });
-                    build.changes = changeList.reverse();
-                    Vue.set(app.builds, i, build);
-                });
-                build.artifacts = build.artifacts.slice(0, 1);
-                build.artifacts.forEach(function (artifact) {
-                    artifact.url = baseUrl + build.number + "/artifact/" + artifact.relativePath;
-                    artifact.type = function () {
-                        if (artifact.fileName.startsWith('paperclip')) {
-                            return "Server";
-                        }
-
-                        return "Unknown";
-                    }
-                })
-            });
-            app.loading = false;
-            app.builds = jenkinsResult.builds;
-        })
-    }
-
-    buildChanges(JENKINS_113_URL, '#downloads-container-113');
-    buildChanges(JENKINS_LEGACY_URL, '#downloads-container-112');
-}
-
-window.addEventListener('load', onLoad);
+        <tbody>
+          ${rows}
+        </tbody>
+      </table>
+      <a class="jenkins-btn btn grey darken-4 waves-effect waves-light" href="https://papermc.io/ci/job/${id}/">More</a>`;
+    }).catch(function() {
+      document.getElementById(id).innerText = "Failed to load downloads.";
+    });
+  }
+  
+  document.getElementById("content").innerHTML = `
+  <div class="col s12">
+    <ul id="downloads-tabs" class="tabs">
+      ${tabs}
+    </ul>
+  </div>
+  ${tabContents}`;
+  
+  M.Tabs.init(document.querySelector("#downloads-tabs"), {});
+});
