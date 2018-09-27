03f0e33a03e1d0c9aa9ebfd518d9235850184789
Aikar <aikar@aikar.co>
2018-09-26 21:55:06 -0400

    Add Google Analytics to site and cache bust resources

---
+++ src/js/site.js
@@ -0,0 +1,8 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-126555192-1');
+var gtmJS = document.createElement('script');
+gtmJS.setAttribute('src','https://www.googletagmanager.com/gtag/js?id=UA-126555192-1');
+document.head.appendChild(gtmJS);
+
