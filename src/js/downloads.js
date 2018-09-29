afdf40c68669c0abb7be1e29be7507b6883f7166
Aikar <aikar@aikar.co>
2018-09-29 17:00:14 -0400

    Final touches

--- src/js/downloads.js
+++ src/js/downloads.js
@@ -1,15 +1,27 @@
 const downloads = {
-    "Paper 1.13.1": {
-        "id": "Paper-1.13",
-        "github": "PaperMC/Paper"
+    "Paper-1.13": {
+        "title": "Paper 1.13.1",
+        "jenkins": "Paper-1.13",
+        "github": "PaperMC/Paper",
+        "desc": "Active development for the current Minecraft version.",
     },
-    "Paper 1.12.2": {
-        "id": "Paper",
-        "github": "PaperMC/Paper"
+    "Paper-1.12": {
+        "title": "Paper 1.12.2",
+        "jenkins": "Paper",
+        "github": "PaperMC/Paper",
+        "desc": "Legacy support for Minecraft 1.12.2, accepting bug and security fixes only."
     },
     "Waterfall": {
-        "id": "Waterfall",
-        "github": "WaterfallMC/Waterfall"
+        "title": "Waterfall",
+        "jenkins": "Waterfall",
+        "github": "PaperMC/Waterfall",
+        "desc": "Our fork of the BungeeCord software, with improved Forge support and more features."
+    },
+    "Travertine": {
+        "title": "Travertine",
+        "jenkins": "Travertine",
+        "github": "PaperMC/Travertine",
+        "desc": "Waterfall, with additional support for Minecraft 1.7.10."
     }
 };
 
@@ -29,15 +41,18 @@ function escapeHTML(str) {
 document.addEventListener('DOMContentLoaded', function () {
     // Show downloads
     let tabs = "", tabContents = "", jobs = Object.keys(downloads).length;
-    for (const title in downloads) {
-        tabs += `<li class="tab col s${12 / jobs}"><a href="#${downloads[title].id}">${title}</a></li>`;
-        const id = downloads[title].id;
+    for (const id in downloads) {
+        const title = downloads[id].title;
+        tabs += `<li class="tab col s${12 / jobs}"><a href="#${id}">${title}</a></li>`;
 
         tabContents += `<div id="${id}" class="col s12">
-      <div class="progress">
-          <div class="indeterminate"></div>
-      </div>
-    </div>`;
+            <div class="download-content">
+            <div class="download-desc">${downloads[id].desc}</div>          
+              <div class="progress">
+                  <div class="indeterminate"></div>
+              </div>
+          </div>
+        </div>`;
 
     }
     document.getElementById("content").innerHTML = `
@@ -48,12 +63,10 @@ document.addEventListener('DOMContentLoaded', function () {
       </div>
       ${tabContents}`;
 
-    for (const title in downloads) {
-        const id = downloads[title].id;
-        const githubID = downloads[title].github;
-        jenkinsFetch(id, "/api/json?tree=builds[number,url,artifacts[fileName,relativePath],timestamp]{,5}").then(function (json) {
-            const container = document.getElementById(id);
-            console.log(id, container, json);
+    for (const id in downloads) {
+        const githubID = downloads[id].github;
+        jenkinsFetch(downloads[id].jenkins, "/api/json?tree=builds[number,url,artifacts[fileName,relativePath],timestamp,changeSet[items[comment,commitId,msg]]]{,10}").then(function (json) {
+            const container = document.getElementById(id).querySelector(".download-content");
             if (json == null) {
                 container.innerText = "Failed to load downloads.dd";
                 return;
@@ -62,15 +75,34 @@ document.addEventListener('DOMContentLoaded', function () {
             let rows = "";
             const builds = json.builds.filter(build => build.artifacts && build.artifacts.length);
             builds.forEach(function (build) {
-                rows += `
-        <tr>
-          <td><a href="https://papermc.io/ci/job/${id}/${build.number}/artifact/${build.artifacts[0].fileName}" class="btn waves-light waves-effect grey darken-4">#${build.number}<i class="material-icons left">save_alt</i></a></td>
-          <td data-build-id="${build.number}">Loading...</td>
-          <td>${new Date(build.timestamp).toLocaleDateString({year: "numeric", month: "numeric", day: "numeric"})}</td>
-        </tr>`;
+
+                const el = container.querySelector("td[data-build-id='" + build.number + "']");
+                if (build == null) {
+                    el.innerText = "Failed to load downloads.";
+                    return;
+                }
+
+                let changes = "";
+                build.changeSet.items.forEach(function (item) {
+                    changes += `<span class="commit-hash">[<a title="${escapeHTML(item.comment)}" href="https://github.com/${githubID}/commit/${item.commitId}" target="_blank">${escapeHTML(item.commitId.substring(0, 7))}</a>]</span> ${escapeHTML(item.msg)}<br>`;
+                });
+
+                if (!changes) {
+                    changes = "No changes";
+                }
+
+                rows += `<tr>
+                  <td><a href="https://papermc.io/ci/job/${id}/${build.number}/artifact/${build.artifacts[0].fileName}" 
+                  class="btn waves-light waves-effect grey darken-4">
+                  #${build.number}<i class="material-icons left">save_alt</i>
+                  </a></td>
+                  <td data-build-id="${build.number}">${changes}</td>
+                  <td>${new Date(build.timestamp).toLocaleDateString({year: "numeric", month: "numeric", day: "numeric"})}</td>
+                </tr>`;
             });
 
             container.innerHTML = `
+                <div class="download-desc">${downloads[id].desc}</div>
               <table class="builds-table">
                 <thead>
                   <tr>
@@ -86,26 +118,6 @@ document.addEventListener('DOMContentLoaded', function () {
               </table>
               <a class="jenkins-btn btn blue darken-4 waves-effect waves-light" href="https://papermc.io/ci/job/${id}/">More</a>
             `;
-
-            builds.forEach(build => {
-                jenkinsFetch(id, "/" + build.number + "/api/json?wrapper=changes").then(function (build) {
-                    const el = container.querySelector("td[data-build-id='" + build.number + "']");
-                    if (build == null) {
-                        el.innerText = "Failed to load downloads.";
-                        return;
-                    }
-
-                    let changes = "";
-                    build.changeSet.items.forEach(function (item) {
-                        changes += `[<a title="${escapeHTML(item.comment)}" href="https://github.com/${githubID}/commit/${item.commitId}" target="_blank">${escapeHTML(item.commitId.substring(0, 7))}</a>] ${escapeHTML(item.msg)}<br>`;
-                    });
-
-                    el.innerHTML = changes ? changes.substr(0, changes.length - 1) : "Changes not known";
-                }).catch(function (e) {
-                    console.error(e);
-                    document.querySelector("td[data-build-id='" + build.number + "']").innerText = "Failed to load downloads.";
-                });
-            });
 
         }).catch(function (e) {
             console.error(e);
---
+++ src/js/site.js
@@ -0,0 +1,53 @@
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){
+    if (!hasLoaded || !consentRevoked) {
+        dataLayer.push(arguments);
+    }
+}
+
+gtag('js', new Date());
+gtag('config', 'UA-126555192-1');
+
+
+function checkConsent(consent) {
+    consentRevoked = !consent;
+    if (consent && !hasLoaded) {
+        const gtmJS = document.createElement('script');
+        gtmJS.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=UA-126555192-1');
+        document.head.appendChild(gtmJS);
+        hasLoaded = true;
+    }
+}
+
+cookieconsent.initialise({
+    container: document.body.getElementsByTagName("main")[0],
+    "palette": {
+        "popup": {
+            "background": "#252e39"
+        },
+        "button": {
+            "background": "#14a7d0"
+        }
+    },
+    "theme": "edgeless",
+    "type": "opt-in",
+    "content": {
+        "message": "PaperMC uses cookies to help analyze how many people use our software.",
+        "allow": "Allow"
+    },
+    revokable: true,
+    onStatusChange(status) {
+        checkConsent(this.hasConsented());
+    },
+    onInitialise(status) {
+        checkConsent(this.hasConsented());
+    },
+    onNoCookieLaw() {
+        checkConsent(true);
+    },
+    law: {
+        regionalLaw: true,
+    },
+    location: true
+});
