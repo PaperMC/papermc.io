429cf3e71084aa708331e5bde9c8ccb313491ec0
Sweepyoface <sweepy@sweepy.pw>
2018-10-30 23:09:34 -0400

    Gave me 4 spaces again

--- gulpfile.js
+++ gulpfile.js
@@ -9,72 +9,78 @@ const gulp_buster = require("gulp-buster");
 const gulp_rename = require("gulp-rename");
 
 gulp.task("sass", function() {
-    return gulp
-        .src("src/css/*.scss")
-        .pipe(
-            gulp_sass({
-                outputStyle: "compressed"
-            }).on("error", gulp_sass.logError)
-        )
-        .pipe(gulp.dest("dist/css"));
+  return gulp
+    .src("src/css/*.scss")
+    .pipe(
+      gulp_sass({
+        outputStyle: "compressed"
+      }).on("error", gulp_sass.logError)
+    )
+    .pipe(gulp.dest("dist/css"));
 });
 
 gulp.task("terse", function() {
-    return gulp
-        .src("src/js/*.js")
-        .pipe(gulp_terser())
-        .pipe(gulp.dest("dist/js"));
+  return gulp
+    .src("src/js/*.js")
+    .pipe(gulp_terser())
+    .pipe(gulp.dest("dist/js"));
 });
 
 gulp.task("copy images", function() {
-    return gulp.src("src/images/*").pipe(gulp.dest("dist/images"));
+  return gulp.src("src/images/*").pipe(gulp.dest("dist/images"));
 });
 
 gulp.task("copy favicons", function() {
-    return gulp.src("src/favicons/*").pipe(gulp.dest("dist/favicons"));
+  return gulp.src("src/favicons/*").pipe(gulp.dest("dist/favicons"));
 });
 
 gulp.task("bust", function() {
-    return gulp
-        .src("dist/**/*")
-        .pipe(gulp_buster({algo: "sha256", length: 16}))
-        .pipe(gulp.dest("dist"))
+  return gulp
+    .src("dist/**/*")
+    .pipe(gulp_buster({
+      algo: "sha256",
+      length: 16
+    }))
+    .pipe(gulp.dest("dist"));
 });
 
 gulp.task("content", function() {
-    return new Promise((resolve, reject) => {
-        let busters = JSON.parse(fs.readFileSync("dist/busters.json"));
+  return new Promise((resolve, reject) => {
+    let busters = JSON.parse(fs.readFileSync("dist/busters.json"));
 
-        var twigData = {};
+    var twigData = {};
 
-        Object.keys(busters).forEach(file => {
-
-            let baseName = path.basename(file);
-            let dir = path.dirname(file).split("/")[1]
-            twigData[baseName.replace(/-/g, "__").replace(/\./g, "_")] = dir + "/" + baseName + "?v=" + busters[file];
-        });
-
-        gulp.src("src/*.twig")
-            .pipe(gulp_twig({data: twigData}))
-            .on("error", reject)
-            .pipe(
-                gulp_rename(path => {
-                    path.extname = ".html";
-                })
-            )
-            .pipe(gulp.dest("dist"))
-            .on("end", resolve);
+    Object.keys(busters).forEach(file => {
+      let baseName = path.basename(file);
+      let dir = path.dirname(file).split("/")[1];
+      twigData[baseName.replace(/-/g, "__").replace(/\./g, "_")] = dir + "/" + baseName + "?v=" + busters[file];
     });
+
+    gulp.src("src/*.twig")
+      .pipe(gulp_twig({
+        data: twigData
+      }))
+      .on("error", reject)
+      .pipe(
+        gulp_rename(path => {
+          path.extname = ".html";
+        })
+      )
+      .pipe(gulp.dest("dist"))
+      .on("end", resolve);
+  });
 });
 
 gulp.task("minify", function() {
-    return gulp
-        .src("dist/*.html")
-        .pipe(gulp_htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest("dist"));
+  return gulp
+    .src("dist/*.html")
+    .pipe(gulp_htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest("dist"));
 });
 
 gulp.task(
-    "default",
-    gulp.series("sass", "terse", "copy images", "copy favicons", "bust", "content", "minify")
+  "default",
+  gulp.series("sass", "terse", "copy images", "copy favicons", "bust", "content", "minify")
 );
