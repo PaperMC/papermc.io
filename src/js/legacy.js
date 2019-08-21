32c032019b6e815bc11c5abd8a645b0f114de1e9
darbyjack <admin@glaremasters.me>
2019-08-21 10:18:36 -0500

    [W.I.P] Dynamically generate HTML

--- src/js/legacy.js
+++ src/js/legacy.js
@@ -1,6 +1,21 @@
 const versions = ["1.12.2", "1.11.2", "1.10.2", "1.9.4", "1.8.8"];
-
-versions.forEach(function(i) {
-  fetch('/api/v1/paper/' + i + '/latest').then(r => r.json())
-  .then(data => document.getElementById(i).href="https://papermc.io/api/v1/paper/" + i + "/" + data.build + "/download");
+var div = document.getElementById('content');
+document.addEventListener('DOMContentLoaded', () => {
+	var text = '';
+	versions.forEach(function(i) {
+		fetch('/api/v1/paper/' + i + '/latest').then(r => r.json()).then(data => {
+			text += `<div class="row">
+      <div class="col s12 l3">
+        <i class="material-icons benefit-icon">assignment_late</i>
+      </div>
+      <p></p>
+      <div class="col s12 l9">
+        <h4>${i}</h4>
+        <p><strong>This build is purely for accessability. By clicking the download button, you acknowledge that no support will be provided whatsoever.</strong></p>
+        <a id="${i}" href="https://papermc.io/api/v1/paper/${i}/${data.build}/download" class="waves-effect waves-light btn light-blue darken-2">
+          Download Anyways</a>
+      </div>
+    </div>`
+		}).then(() => div.innerHTML = text);
+	});
 });
