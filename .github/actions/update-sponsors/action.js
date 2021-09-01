a8561c4fc6a99b13beae4b426695660b47f96cae
MiniDigger <admin@benndorf.dev>
2021-09-01 14:16:13 +0200

    Add sponsors page + action to update it

---
+++ .github/actions/update-sponsors/action.js
@@ -0,0 +1,70 @@
+const core = require('@actions/core');
+const fs = require('fs');
+const axios = require('axios');
+
+main();
+
+async function main() {
+    const [ocData, ghData] = await Promise.all([opencollective(), github()]);
+    console.log(`Found ${ocData.collective.contributors.totalCount} OC Sponsors and ${ghData.organization.sponsors.totalCount} GH Sponsors`);
+    fs.writeFileSync('/home/runner/work/papermc.io/papermc.io/work/sponsors.json', JSON.stringify({ocData, ghData}));
+    console.log('Saved');
+}
+
+async function opencollective() {
+    // language=GraphQL
+    const query = `{
+        collective(slug: "papermc") {
+            name
+            slug
+            stats {
+                balance {
+                    valueInCents
+                }
+                monthlySpending {
+                    valueInCents
+                }
+            }
+            contributors(roles: BACKER) {
+                totalCount
+                nodes {
+                    name
+                    image
+                }
+            }
+        }
+    }`;
+    const result = await graphQL('https://api.opencollective.com/graphql/v2', query);
+    return result.data;
+}
+
+async function github() {
+    // language=GraphQL
+    const query = `{
+        organization(login: "papermc") {
+            sponsors(first: 100) {
+                totalCount
+                nodes {
+                    ... on Actor {
+                        login
+                        avatarUrl
+                    }
+                }
+            }
+        }
+    }`;
+    const apiKey = core.getInput("repo-token", {required: true});
+    const result = await graphQL('https://api.github.com/graphql', query, 'Bearer ' + apiKey); // dummy account with no perms
+    return result.data;
+}
+
+async function graphQL(url, query, auth) {
+    const result = await axios.post(url, {query}, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': auth ? auth : ''
+        }
+    });
+    return result.data;
+}
---
+++ src/js/sponsors.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const {ocData, ghData} = await (await fetch("https://raw.githubusercontent.com/PaperMC/papermc.io/data/sponsors.json")).json();
+    const list = document.getElementById("sponsor-list");
+
+    ocData.collective.contributors.nodes.forEach(node => {
+        list.appendChild(createListEntry(node.name, node.image));
+    });
+    ghData.organization.sponsors.nodes.forEach(node => {
+        list.appendChild(createListEntry(node.login, node.avatarUrl));
+    });
+
+    document.getElementById("oc-balance").innerText = '$' + (ocData.collective.stats.balance.valueInCents / 100);
+    document.getElementById("oc-spending").innerText = '$' + (ocData.collective.stats.monthlySpending.valueInCents / 100);
+});
+
+function createListEntry(name, image) {
+    const li = document.createElement("li");
+    li.innerHTML = `
+            <img src='${image}' alt='${name}' title='${name}' onerror='this.src="https://opencollective.com/static/images/default-guest-logo.svg"'/>
+        `;
+    return li;
+}
+
