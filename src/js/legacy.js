ceffaa3639610334aaf1a2517f6480ac303a4768
Nassim Jahnke <jahnke.nassim@gmail.com>
2021-12-27 20:02:42 +0100

    Make the legacy questionnaire slightly less aggressive

--- src/js/legacy.js
+++ src/js/legacy.js
@@ -53,19 +53,14 @@ timer = setInterval(
 function checkValue(id, value) {
   const element = document.getElementById(id);
   const elementValue = element.options[element.selectedIndex].value;
-
-  if (value === elementValue) {
-    return true;
-  } else {
-    return false;
-  }
+  return value === elementValue;
 }
 
 submitButton.onclick = function() {
   submitButton.disabled = true;
   let failed = false;
 
-  if (!checkValue("quiz-1", "2") || !checkValue("quiz-2", "2") || !checkValue("quiz-3", "2") || !checkValue("quiz-4", "4")) {
+  if (!checkValue("quiz-1", "2") || !checkValue("quiz-2", "2")) {
     failed = true;
   }
 
