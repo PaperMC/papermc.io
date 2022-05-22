7816a8be934b0eb19b1fa0a1e50e3da39985e0bb
MiniDigger | Martin <admin@benndorf.dev>
2022-05-22 12:09:42 +0200

    update deps

--- src/js/sponsors.js
+++ src/js/sponsors.js
@@ -4,15 +4,23 @@ document.addEventListener('DOMContentLoaded', async () => {
     const list = document.getElementById("sponsor-list");
 
     ocData.collective.contributors.nodes.forEach(node => {
-        list.appendChild(createListEntry(node.name, node.image));
+        list.appendChild(createListEntry(clean(node.name), node.image));
     });
     ghData.organization.sponsors.nodes.forEach(node => {
-        list.appendChild(createListEntry(node.login, node.avatarUrl));
+        list.appendChild(createListEntry(clean(node.login), node.avatarUrl));
     });
 
     document.getElementById("oc-balance").innerText = '$' + (ocData.collective.stats.balance.valueInCents / 100);
     document.getElementById("oc-spending").innerText = '$' + (ocData.collective.stats.monthlySpending.valueInCents / 100);
 });
+
+function clean(name) {
+    return name.replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#039;');
+}
 
 function createListEntry(name, image) {
     const li = document.createElement("li");
