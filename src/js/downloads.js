180d463d282be67dc6f5eed7b27ee320d32b11da
Shane Freeder <theboyetronic@gmail.com>
2022-07-27 23:33:04 +0100

    1.19 -> 1.19.1

--- src/js/downloads.js
+++ src/js/downloads.js
@@ -1,10 +1,10 @@
 const downloads = {
     "Paper-1.19": {
-        "title": "Paper 1.19",
+        "title": "Paper 1.19.1",
         "api_endpoint": "paper",
         "api_version": "1.19",
         "github": "PaperMC/Paper",
-        "desc": "Active development for Minecraft 1.19",
+        "desc": "Active development for Minecraft 1.19.1",
         "limit": 10,
         "cache": null,
     },
